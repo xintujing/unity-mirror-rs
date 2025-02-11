d5357ef71574913e3193e286f74fed57035b3b06
Caozhigang <caozhigang8@126.com>
2025-02-11 11:13:59 +0800

    feat(core): 实现远程过程调用功能
    - 新增 RemoteProcedureCalls 结构体和相关方法- 实现命令注册功能，支持动态添加函数
    - 添加 REGISTER_FUNCTIONS 全局变量用于存储注册的函数
    - 实现函数调用逻辑，可在运行时动态调用注册的函数

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// mod mirror;
+// mod unity;
+//
+// mod demos;
+
+pub struct RemoteProcedureCalls;
+
+pub(crate) static mut REGISTER_FUNCTIONS: Vec<fn()> = vec![];
+
+pub trait Type {}
+pub trait RemoteCallDelegate {}
+
+impl RemoteProcedureCalls {
+    pub fn register_command(
+        component_type: Option<()>,
+        function_full_name: String,
+        func: Option<fn()>,
+        requires_authority: bool,
+    ) {
+        if let Some(_fn) = func {
+            unsafe { REGISTER_FUNCTIONS.push(_fn) }
+        }
+
+        // func.unwrap()()
+        // RegisterDelegate(
+        //     component_type,
+        //     function_full_name,
+        //     RemoteCallType.Command,
+        //     func,
+        //     requires_authority,
+        // );
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn aaa() {
+    unsafe {
+        for x in REGISTER_FUNCTIONS.iter() {
+            x();
+        }
     }
 }
