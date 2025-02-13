5cb7012a303bf7da59da9421d99cb21ad3de027c
Caozhigang <caozhigang8@126.com>
2025-02-13 15:21:32 +0800

    feat(core): 更新 RPC 注册函数签名并添加网络相关结构体 - 更新了 REGISTER_FUNCTIONS 的类型，使其支持接收网络行为对象和读取器 - 添加了 NetworkBehaviour、NetworkReader 和 NetworkConnectionToClient 等网络相关结构体 - 修改了 register_command 函数，使其支持新的 RPC 函数签名

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
-// mod mirror;
-// mod unity;
-//
-// mod demos;
+use std::any::Any;
 
 pub struct RemoteProcedureCalls;
 
-pub(crate) static mut REGISTER_FUNCTIONS: Vec<fn()> = vec![];
+pub(crate) static mut REGISTER_FUNCTIONS: Vec<
+    fn(
+        obj: Box<dyn NetworkBehaviour>,
+        reader: NetworkReader,
+        sender_connection: NetworkConnectionToClient,
+    ),
+> = vec![];
 
 pub trait Type {}
 pub trait RemoteCallDelegate {}
@@ -14,7 +17,13 @@ impl RemoteProcedureCalls {
     pub fn register_command(
         component_type: Option<()>,
         function_full_name: String,
-        func: Option<fn()>,
+        func: Option<
+            fn(
+                obj: Box<dyn NetworkBehaviour>,
+                reader: NetworkReader,
+                sender_connection: NetworkConnectionToClient,
+            ),
+        >,
         requires_authority: bool,
     ) {
         if let Some(_fn) = func {
@@ -32,10 +41,34 @@ impl RemoteProcedureCalls {
     }
 }
 
-pub fn aaa() {
-    unsafe {
-        for x in REGISTER_FUNCTIONS.iter() {
-            x();
-        }
+pub trait NetworkBehaviour {
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+}
+
+pub struct NetworkReader {
+    data: Vec<u8>,
+    position: usize,
+}
+
+impl NetworkReader {
+    pub fn read<T>(&mut self) -> T {
+        todo!()
     }
+}
+
+pub struct NetworkConnectionToClient {
+    // network_connection: NetworkConnection,
+    // pub reliable_rpcs_batch: NetworkWriter,
+    // pub unreliable_rpcs_batch: NetworkWriter,
+    // pub address: String,
+    // pub observing: Vec<u32>,
+    // pub drift_ema: ExponentialMovingAverage,
+    // pub delivery_time_ema: ExponentialMovingAverage,
+    // pub remote_timeline: f64,
+    // pub remote_timescale: f64,
+    // pub buffer_time_multiplier: f64,
+    // pub buffer_time: f64,
+    // pub snapshots: BTreeMap<OrderedFloat<f64>, TimeSnapshot>,
+    // pub snapshot_buffer_size_limit: i32,
+    // pub _rtt: ExponentialMovingAverage,
 }
