4cedf3577a0da599ba4e06bef8a01cc382fd9bf0
aurorax-neo <15047150695@163.com>
2025-05-21 17:23:42 +0800

    refactor(network_behaviour): enhance sync variable getter/setter and on-change callback implementation

--- unity-mirror-rs-macro/src/network_behaviour.rs
+++ unity-mirror-rs-macro/src/network_behaviour.rs
@@ -40,7 +40,11 @@ pub(crate) fn handler(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let mut item_struct = syn::parse_macro_input!(item as syn::ItemStruct);
 
+    // struct_ident
     let struct_ident = &item_struct.ident;
+
+    let state_condition_ident = format_ident!("Base{}", struct_ident);
+    let on_change_callback = format_ident!("{}OnChangeCallback", struct_ident);
 
     match item_struct.fields {
         Fields::Named(_) => {}
@@ -73,7 +77,7 @@ pub(crate) fn handler(attr: TokenStream, item: TokenStream) -> TokenStream {
             if attr.path().is_ident("sync_variable") {
                 // 修改字段的可见性
                 field.vis = syn::Visibility::Inherited;
-                sync_var_fields.push(field.ident.clone().unwrap());
+                sync_var_fields.push((field.ident.clone().unwrap(), field.ty.clone()));
                 break;
             }
         }
@@ -90,10 +94,13 @@ pub(crate) fn handler(attr: TokenStream, item: TokenStream) -> TokenStream {
     // //
     let mut serialize_sync_var_ts = Vec::new();
     let mut deserialize_sync_var_ts = Vec::new();
+    let mut sync_variable_getter_setter = vec![];
+    let mut on_change_callback_ts = Vec::new();
 
-    for (field_index, field) in sync_var_fields.iter().enumerate() {
+    for (field_index, (field, field_type)) in sync_var_fields.iter().enumerate() {
         let get_sync_field_ident = format_ident!("get_{}", field);
         let set_sync_field_ident = format_ident!("set_{}", field);
+        let on_change_callback_ident = format_ident!("on_{}_changed", field);
         serialize_sync_var_ts.push(quote! {
             if initial_state || (dirty_bits & (1u64 << (self.var_start_offset + #field_index as u8))) != 0 {
                 self.#field.serialize(writer);
@@ -102,8 +109,49 @@ pub(crate) fn handler(attr: TokenStream, item: TokenStream) -> TokenStream {
 
         deserialize_sync_var_ts.push(quote! {
             if initial_state || (dirty_bits & (1u64 << (self.var_start_offset + #field_index as u8))) != 0 {
-                // self.#set_sync_field_ident(reader.deserialize());
+                self.#set_sync_field_ident(<#field_type as crate::mirror::network_reader::DataTypeDeserializer>::deserialize(reader));
             }
+        });
+
+        sync_variable_getter_setter.push(quote! {
+            pub fn #get_sync_field_ident(&self) -> &#field_type {
+                &self.#field
+            }
+
+            pub fn #set_sync_field_ident(&mut self, value: #field_type) {
+
+                 let old_value = unsafe {
+                    let mut value_buffer = [0u8; size_of::<#field_type>()];
+                    std::ptr::copy_nonoverlapping(
+                        &self.#field as *const #field_type as *const u8,
+                        value_buffer.as_mut_ptr(),
+                        size_of::<#field_type>(),
+                    );
+                    std::mem::transmute::<[u8; size_of::<#field_type>()], #field_type>(value_buffer)
+                };
+
+                let new_value = unsafe {
+                    let mut value_buffer = [0u8; size_of::<#field_type>()];
+                    std::ptr::copy_nonoverlapping(
+                        &value as *const #field_type as *const u8,
+                        value_buffer.as_mut_ptr(),
+                        size_of::<#field_type>(),
+                    );
+                    std::mem::transmute::<[u8; size_of::<#field_type>()], #field_type>(value_buffer)
+                };
+
+                self.#field = value;
+
+                if let Some(mut network_behaviour) = self.parent.get() {
+                    network_behaviour.sync_var_dirty_bits |= 1u64 << (self.var_start_offset + #field_index as u8);
+                }
+
+                self.#on_change_callback_ident(&old_value, &new_value)
+            }
+        });
+
+        on_change_callback_ts.push(quote! {
+            fn #on_change_callback_ident(&mut self, old_value: &#field_type, new_value: &#field_type){}
         });
     }
 
@@ -374,16 +422,34 @@ pub(crate) fn handler(attr: TokenStream, item: TokenStream) -> TokenStream {
                     }
 
                     if let Some(mut network_behaviour) = self.parent.get() {
+                        use crate::mirror::network_reader::DataTypeDeserializer;
+                        let mut dirty_bits = 0;
                         if initial_state{
+                            #(#deserialize_sync_var_ts)*
                             return;
                         }
                         network_behaviour.sync_var_dirty_bits = reader.read_blittable::<u64>();
+                        dirty_bits = network_behaviour.sync_var_dirty_bits;
+                        #(#deserialize_sync_var_ts)*
                     }
                 }
+            }
+
+            // 同步变量 get/set
+            impl #struct_ident {
+                #(#sync_variable_getter_setter)*
             }
         }
 
         pub use #this_struct_private_mod_ident::#struct_ident;
+
+        trait #state_condition_ident: #on_change_callback {}
+
+        trait #on_change_callback {
+            #(#on_change_callback_ts)*
+        }
+
+        impl #state_condition_ident for #struct_ident {}
 
         // impl crate::mirror::network_behaviour_trait::NetworkBehaviourInstance for #struct_ident {
         //     fn instance(weak_game_object: RevelWeak<GameObject>, metadata: &MetadataNetworkBehaviourWrapper) -> (Vec<(RevelArc<Box<dyn MonoBehaviour>>, TypeId)>, RevelWeak<crate::mirror::NetworkBehaviour>, u8, u8)
