2fabfc900334c2b395c059a73c0c156207488129
aurorax-neo <15047150695@163.com>
2025-06-03 10:04:46 +0800

    refactor(network_transform): implement serialization and deserialization for NetworkTransformUnreliable

--- src/mirror/components/network_transform/network_transform_unreliable.rs
+++ src/mirror/components/network_transform/network_transform_unreliable.rs
@@ -3,14 +3,22 @@ use crate::metadata_settings::mirror::network_behaviours::metadata_network_trans
 use crate::mirror::components::network_transform::network_transform_base::NetworkTransformBase;
 use crate::mirror::components::network_transform::transform_snapshot::TransformSnapshot;
 use crate::mirror::network_behaviour::TNetworkBehaviour;
+use crate::mirror::network_reader::NetworkReader;
+use crate::mirror::network_writer::NetworkWriter;
+use crate::mirror::{
+    NetworkBehaviourOnDeserializer, NetworkBehaviourOnSerializer, TBaseNetworkBehaviour,
+};
 use crate::unity_engine::MonoBehaviour;
 use crate::unity_engine::Time;
-use unity_mirror_macro::{namespace, network_behaviour};
+use unity_mirror_macro::{
+    ancestor_on_deserialize, ancestor_on_serialize, namespace, network_behaviour,
+};
 
 #[namespace(prefix = "Mirror")]
 #[network_behaviour(
     parent(NetworkTransformBase),
-    metadata(MetadataNetworkTransformUnreliable)
+    metadata(MetadataNetworkTransformUnreliable),
+    not_impl_nos
 )]
 pub struct NetworkTransformUnreliable {
     pub buffer_reset_multiplier: f32,
@@ -89,4 +97,13 @@ impl TNetworkBehaviour for NetworkTransformUnreliable {
     {
         Self::default()
     }
+}
+
+impl NetworkBehaviourOnSerializer for NetworkTransformUnreliable {
+    #[ancestor_on_serialize]
+    fn on_serialize(&mut self, writer: &mut NetworkWriter, initial_state: bool) {}
+}
+impl NetworkBehaviourOnDeserializer for NetworkTransformUnreliable {
+    #[ancestor_on_deserialize]
+    fn on_deserialize(&mut self, reader: &mut NetworkReader, initial_state: bool) {}
 }
