36cb58ee8555e3923c93bdcbd210ac9e5218904a
aurorax-neo <15047150695@163.com>
2025-06-23 11:44:18 +0800

    refactor(network_manager_factory.rs): update import paths for NetworkManagerInstance and NetworkManagerFactory

--- unity-mirror-macro-rs/src/network_manager_factory.rs
+++ unity-mirror-macro-rs/src/network_manager_factory.rs
@@ -30,8 +30,8 @@ pub(crate) fn handler(item: TokenStream) -> TokenStream {
         #[ctor::ctor]
         #[inline]
         fn #register_cotr_fn_ident() {
-            use crate::mirror::NetworkManagerInstance;
-            crate::mirror::NetworkManagerFactory::register::<#struct_ident>(
+            use unity_mirror_rs::mirror::NetworkManagerInstance;
+            unity_mirror_rs::mirror::NetworkManagerFactory::register::<#struct_ident>(
                 #struct_ident::instance,
             );
         }
