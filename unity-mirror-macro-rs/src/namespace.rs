b4ff390890838886e55cf8e1b778214113dbde48
aurorax-neo <15047150695@163.com>
2025-07-01 09:48:52 +0800

    refactor(namespace.rs): enhance handler to support both structs and enums

--- unity-mirror-macro-rs/src/namespace.rs
+++ unity-mirror-macro-rs/src/namespace.rs
@@ -1,7 +1,7 @@
 use crate::NamespaceArgs;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{ItemStruct, parse_macro_input};
+use syn::{parse_macro_input, Item};
 impl NamespaceArgs {
     pub(crate) fn get_full_name(&self, struct_ident: &syn::Ident) -> String {
         let prefix = match &self.prefix {
@@ -29,24 +29,51 @@ impl NamespaceArgs {
 pub(crate) fn handler(attr: TokenStream, input: TokenStream) -> TokenStream {
     // 解析属性参数
     let namespace_args = parse_macro_input!(attr as NamespaceArgs);
-    // 结构体
-    let item_struct = parse_macro_input!(input as ItemStruct);
-    // 结构体的标识符
-    let struct_ident = &item_struct.ident;
-    // 结构体的命名空间
-    let full_name = namespace_args.get_full_name(struct_ident);
-
-    quote! {
-        #item_struct
-
-        impl Object for #struct_ident {
-            fn get_full_name() -> &'static str
-            where
-                Self: Sized,
-            {
-                #full_name
+    // 解析输入的结构体或枚举
+    let item = parse_macro_input!(input as Item);
+    match item {
+        // 如果是结构体
+        Item::Struct(item_struct) => {
+            // 结构体的标识符
+            let struct_ident = &item_struct.ident;
+            // 结构体的命名空间
+            let full_name = namespace_args.get_full_name(struct_ident);
+            quote! {
+                #item_struct
+                impl Object for #struct_ident {
+                    fn get_full_name() -> &'static str
+                    where
+                        Self: Sized,
+                    {
+                        #full_name
+                    }
+                }
             }
+                .into()
+        }
+        // 如果是枚举
+        Item::Enum(item_enum) => {
+            // 枚举的标识符
+            let enum_ident = &item_enum.ident;
+            // 枚举的命名空间
+            let full_name = namespace_args.get_full_name(enum_ident);
+            quote! {
+                #item_enum
+                impl Object for #enum_ident {
+                    fn get_full_name() -> &'static str
+                    where
+                        Self: Sized,
+                    {
+                        #full_name
+                    }
+                }
+            }
+                .into()
+        }
+        // 对其他类型报错
+        _ => {
+            let error = "This macro only supports structs and enums.".to_string();
+            syn::Error::new_spanned(item, error).to_compile_error().into()
         }
     }
-    .into()
 }
