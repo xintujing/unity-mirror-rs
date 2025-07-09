6fcca18b60286b06115b3b980caae0012e231d33
aurorax-neo <15047150695@163.com>
2025-07-09 14:23:55 +0800

    refactor(network_identity.rs): remove debug log for network behaviours in observer addition

--- src/mirror/network_identity.rs
+++ src/mirror/network_identity.rs
@@ -560,7 +560,6 @@ impl NetworkIdentity {
 
         self.observers.insert(conn.connection_id, conn.downgrade());
         if let Some(self_arc) = self.self_weak.upgrade() {
-            log::debug!("has network_behaviours: {}", self.network_behaviours.len());
             conn.add_to_observing(self_arc)
         }
     }
